a031373fd42057dfd7d28bfe3cdab0bae395a7ce
vijay <vijaydurkikar1@gmail.com>
2023-02-07 08:33:45 +0530

    [vijay]Add:Attendance

---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Employee
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee empWage = new Employee();
+            empWage.Attendance();
+            Console.ReadLine();
+        }
+    }
+}
